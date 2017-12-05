cfe3ef28a7dde1cf2d6f3aef3c96ba68c621fe33
Trevor Jaskot <trevor186@msn.com>
2017-12-05 11:55:41 -0500

    initial commit

---
+++ app/src/main/java/com/example/trevor/hillbilliesbarn/DoubleUp.java
@@ -0,0 +1,14 @@
+package com.example.trevor.hillbilliesbarn;
+
+/**
+ * Created by Trevor on 11/14/2017.
+ */
+
+public class DoubleUp {
+
+    public double myprobability;
+    public static void main(String args[]){
+      //this is where i put the probability code
+    }
+
+}
---
+++ app/src/main/java/com/example/trevor/hillbilliesbarn/hayBail.java
@@ -0,0 +1,12 @@
+package com.example.trevor.hillbilliesbarn;
+
+/**
+ * Created by Trevor on 10/23/2017.
+ */
+
+public class hayBail {
+
+    public Integer weight;
+    public Integer size;
+
+}
